2f8631ec297314bd756addf283fa4b1232282f0f
salmansalmanov <salmanov2904@gmail.com>
2026-09-13 12:28:21 +0400

    feat(ms-product): implement category entity

---
+++ ms-product/src/main/java/com/salman/msproduct/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.salman.msproduct.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+    private Instant deletedAt;
+}
---
+++ ms-product/src/main/java/com/salman/msproduct/entity/Category.java
@@ -0,0 +1,20 @@
+package com.salman.msproduct.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category extends BaseEntity {
+    private String name;
+}
